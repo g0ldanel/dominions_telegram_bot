4a15d60ccefdc69b8c9fb83d5fa43283ebefa625
g0ldan <goldanel@gmail.com>
2020-01-19 16:39:44 +0000

    time for DRY controller actions now with 3 games; support for 1026;still doing manually to prevent hacking filesystem

--- app/controllers/dominions4bot_controller.rb
+++ app/controllers/dominions4bot_controller.rb
@@ -2,14 +2,14 @@ class Dominions4botController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
   require 'byebug'
   before_action :connect_db
-  before_action :set_username  
+  before_action :set_username
   NUMERICAL_KEYBOARD = [
         ["1", "2", "3"],
         ["4", "5", "6"],
         ["7", "8", "9"],
         ["0"]
       ]
-  
+
 
   NATIONS = {"Rag" => "Ragha", "Jom" => "Jomon", "Ut" => "Utgard", "Bog" => "Bogarus", "Rl" => "R'lyeh", "Lem" => "Lemuria", "Rap" => "Caelum", "DT" => "C'Tis", "BK" => "T'ien Ch'i"    }
 
@@ -31,72 +31,70 @@ class Dominions4botController < Telegram::Bot::UpdatesController
     respond_with :message, text: "Hola #{@username} eres #{nation} en #{game} "
   end
 
-  def im_playing_in_1024!
-    players = playing_nations 1024
+
+
+  def im_playing_in(port)
+    players = playing_nations port
     answers = []
     players.each do |player|
-      answers << [{text: "Soy #{player}", callback_data: "soy! #{player}@1024"}]
+      answers << [{text: "Soy #{player}", callback_data: "soy! #{player}@#{port}"}]
     end
-    
-    respond_with :message, text: "¿quién eres en 1024?", reply_markup: {inline_keyboard:  answers}
 
+    respond_with :message, text: "¿quién eres en #{port}?", reply_markup: {inline_keyboard:  answers}
+
+  end
+
+
+  def im_playing_in_1024!
+    self.im_playing_in 1024
   end
 
   def im_playing_in_1025!
-    players = playing_nations 1025
-    answers = []
-    players.each do |player|
-      answers << [{text: "Soy #{player}", callback_data: "soy! #{player}@1025"}]
-    end
-    
-    respond_with :message, text: "¿quién eres en 1025?", reply_markup: {inline_keyboard:  answers}
+    self.im_playing_in 1025
+  end
 
+  def im_playing_in_1026!
+    self.im_playing_in 1026
   end
 
 
-  def status_1024!
-    game_status = ''    
-    lines = File.readlines("/tmp/1024.log", chomp: true).last(2)
+  def status(port)
+    game_status = ''
+    lines = File.readlines("/tmp/#{port}.log", chomp: true).last(2)
     game_status << lines.first
     lines[1].scan(/([A-Z]{1,2}[a-z]{0,}[-?*+])/).each do |player|
       nation = player.last[0...-1]
-      pg = PlayerGame.find_by nation: nation, game: 1024
+      pg = PlayerGame.find_by nation: nation, game: port
       nation_status =  player_status(player.last.last.last)
       unless pg.nil? || nation_status == "Jugado"
-        nation += " @#{pg.username}" 
+        nation += " @#{pg.username}"
       end
       game_status << "\n *#{nation_name(nation)}:* #{nation_status}"
     end
 
     respond_with :message, text: game_status, parse_mode: :Markdown
 
+  end
+
+  def status_1024!
+    self.status 1024
   end
 
   def status_1025!
-    game_status = ''    
-    lines = File.readlines("/tmp/1025.log", chomp: true).last(2)
-    game_status << lines.first
-    lines[1].scan(/([A-Z]{1,2}[a-z]{0,}[-?*+])/).each do |player|
-      nation = player.last[0...-1]
-      pg = PlayerGame.find_by nation: nation, game: 1025
-      nation_status =  player_status(player.last.last.last)
-      unless pg.nil? || nation_status == "Jugado"
-        nation += " @#{pg.username}" 
-      end
-      game_status << "\n *#{nation_name(nation)}:* #{nation_status}"
-    end
+    self.status 1025
+  end
 
-    respond_with :message, text: game_status, parse_mode: :Markdown
-
+  def status_1026!
+    self.status 1026
   end
 
   def read_dat_map
-    
+
   end
 
-  
+
   def action_missing(action, *_args)
-    respond_with :message, text: "Eso no se que es" 
+    respond_with :message, text: "Eso no se que es"
   end
 
 
@@ -120,9 +118,9 @@ class Dominions4botController < Telegram::Bot::UpdatesController
   def player_status(status)
     case(status)
     when "+"
-      "Jugado" 
+      "Jugado"
     when "?"
-      "A medias" 
+      "A medias"
     when "-"
       "Pendiente"
     when "*"
@@ -158,9 +156,9 @@ class Dominions4botController < Telegram::Bot::UpdatesController
 
   def set_username
     begin
-      @username =  update["message"]["from"]["username"] 
+      @username =  update["message"]["from"]["username"]
     rescue
-      @username =  update["callback_query"]["from"]["username"]       
+      @username =  update["callback_query"]["from"]["username"]
     end
   end
 end
