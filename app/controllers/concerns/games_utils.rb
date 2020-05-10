26b96e2f9cad4a414079a5895c20944b5da36776
g0ldan <goldanel@gmail.com>
2020-05-10 22:36:09 +0100

    who_is_who implemented

--- app/controllers/concerns/games_utils.rb
+++ app/controllers/concerns/games_utils.rb
@@ -2,6 +2,40 @@ module GamesUtils
   extend ActiveSupport::Concern
   PORTS = (1024..1029).to_a
 
+
+  def select_game!
+    games = PORTS.map do |port|
+      if Game.exists? port: port
+        [{text: "Borrar #{port}", callback_data: "delete_game! #{port}"},{text: "Who is who in #{port}?", callback_data: "who_is_who #{port}" }]
+      else
+        [{text: "Who is who in #{port}?", callback_data: "who_is_who #{port}" },{text: "Crear #{port}", callback_data: "create_game! #{port}"},]
+      end
+    end
+
+    respond_with :message, text: "Selecciona:\n _Borrar una partida del bot solo hara que deje de leer su estado_", reply_markup: {inline_keyboard:  games}, parse_mode: :Markdown
+  end
+
+
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  #--- who is who
+
+  def who_is_who(port)
+    game = Game.find_by port: port
+byebug
+    unless game.nil?
+      answer = game.player_games.map do |pg|
+        game_status << "*#{nation_name(pg.nation)}:* @#{pg.player.username}\n"
+      end
+      respond_with :message, text: answer, parse_mode: :Markdown
+    else
+      respond_with :message, text: "Payo, no veo a nadie para esa partida (#{port})", parse_mode: :Markdown
+    end
+  end
+
+
+  #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  #--- handle Games
+
   def create_game!(port)
     status = game_status(port).split(', ')
     game = Game.create name: status[0], port: port, era: 'early'
@@ -21,18 +55,6 @@ module GamesUtils
     end
   end
 
-
-  def select_game!
-    games = PORTS.map do |port|
-      if Game.exists? port: port
-        [{text: "Borrar #{port}", callback_data: "delete_game! #{port}"},{text: '-', callback_data: 'nope'}]
-      else
-        [{text: '-', callback_data: 'nope'},{text: "Crear #{port}", callback_data: "create_game! #{port}"},]
-      end
-    end
-
-    respond_with :message, text: "Selecciona:\n _Borrar una partida del bot solo hara que deje de leer su estado_", reply_markup: {inline_keyboard:  games}, parse_mode: :Markdown
-  end
 
 
 
