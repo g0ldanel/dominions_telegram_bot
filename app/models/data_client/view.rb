3da1d972050175503b784d40ab6841132fc0aed7
Abby Henríquez Tejera <paradoja@gmail.com>
2021-02-06 18:59:37 +0100

    Don't show gem cost if it's zero

--- app/models/data_client/view.rb
+++ app/models/data_client/view.rb
@@ -41,7 +41,7 @@ module DataClient
     end
 
     def from_gem_cost(gc)
-      return if gc.nil?
+      return if gc.nil? || gc.amount.zero?
 
       n = from_magic_path_brief(gc.magicPath.name)
       "#{n}#{gc.amount}"
@@ -50,12 +50,18 @@ module DataClient
     def from_spell(s)
       nations = s.restrictedToNations.map(&method(:from_nation_brief)).join(', ')
       n = (nations.length.nil? || nil) && "Restricted to: #{nations}"
+      gem_cost = from_gem_cost(s.gemCost)
+      gem_cost_text = "Cost: #{gem_cost}" if gem_cost
 
       [["*#{s.name}* (_#{s.id}_)",
         from_magic_school_level(s.magicSchoolLevel)].compact.join(', '),
        s.magicPathLevels.map(&method(:from_magic_path_level)).join,
-       ["Fatigue: #{s.fatigueCost}", "Dam: #{s.damage}",
-        "Precision: #{s.precision}", "Cost: #{from_gem_cost(s.gemCost)}"].compact.join(', '),
+
+       ["Fatigue: #{s.fatigueCost}",
+        "Dam: #{s.damage}",
+        "Precision: #{s.precision}",
+        gem_cost_text].compact.join(', '),
+
        n,
        [s.description, s.details && "_Details:_\n#{s.details}"].map do |str|
          str.to_s.split("\n").map(&:strip)
